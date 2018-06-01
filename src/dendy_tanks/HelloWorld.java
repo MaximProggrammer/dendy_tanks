e10461b633d278d1046ffdcb99a5dc8b7d34bd9e
maxim <maxrocers@gmail.com>
2018-06-01 18:19:07 +0600

    Порверили что ресурсы берутся из исходников.

--- src/dendy_tanks/HelloWorld.java
+++ src/dendy_tanks/HelloWorld.java
@@ -1,7 +1,10 @@
 package dendy_tanks;
 
+import java.io.InputStream;
+
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("123");
+    InputStream resourceAsStream = HelloWorld.class.getResourceAsStream("asd.txt");
+    System.out.println(resourceAsStream);
   }
 }
