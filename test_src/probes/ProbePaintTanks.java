f34f1cd656ca84f3ec18014e6e3d2b95140a84ff
maxim <maxrocers@gmail.com>
2018-06-04 19:14:57 +0600

    Научились рисовать танчики.

--- test_src/probes/ProbePaintTanks.java
+++ test_src/probes/ProbePaintTanks.java
@@ -8,17 +8,18 @@ import java.io.File;
 public class ProbePaintTanks {
     public static void main(String[] args) throws Exception{
 
-        BufferedImage tanks = ImageIO.read(ProbePaintTanks.class.getResourceAsStream("/tanks.png"));
+        BufferedImage tanks = ImageIO.read(ProbePaintTanks.class.getResourceAsStream("/tanks.jpeg"));
 
         BufferedImage holst = new BufferedImage(1024,600,BufferedImage.TYPE_INT_ARGB);
 
         {
             Graphics2D g = (Graphics2D) holst.getGraphics();
 
-            g.setColor(Color.GREEN);
+            g.setColor(Color.WHITE);
             g.fillRect(0,0,holst.getWidth(),holst.getHeight());
 
-            g.drawImage(tanks,10,10,null);
+            g.drawImage(tanks.getSubimage(81,48,14,15),10,30,null);
+            g.drawImage(tanks.getSubimage(81,48,14,15),10,80,null);
 
             g.dispose();
         }
